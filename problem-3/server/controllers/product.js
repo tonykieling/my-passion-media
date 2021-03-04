18bd13df00b5d48ffd14517b3380d3f1861f45ee
Tony Kieling <tony.kieling@gmail.com>
2021-03-04 12:07:47 -0800

    problem 1 and 2 are done, doing problem-3

---
+++ problem-3/server/controllers/product.js
@@ -0,0 +1,11 @@
+class Product {
+  constructor(name, weight, dimensions) {
+    this.name       = name;
+    this.weight     = weight;
+    this.dimensions = dimensions;
+  }
+
+  add(name, weight, dimensions) {
+    constr
+  }
+}
